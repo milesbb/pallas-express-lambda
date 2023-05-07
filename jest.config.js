c5862bad8db0974aa53653d0234f8a3308b4e395
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2023-05-07 07:46:03 +0100

    rootdir

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  roots: ["<rootDir>"],
   preset: "ts-jest",
   testEnvironment: "node",
   moduleFileExtensions: ["ts", "js", "json", "node"],
   transform: {
-    '^.+\\.ts?$': 'ts-jest'
+    "^.+\\.ts?$": "ts-jest",
   },
 };
